66c2d30009c1ceea221d5c0b40eb84ad7b57e51c
yusufadell <youssef.31057404@ics.tanta.edu.eg>
2022-09-21 16:59:15 +0200

    ps1: add hello world example

---
+++ exercises/001-hello/hello.py
@@ -0,0 +1,29 @@
+# 
+# Houston, we have a problem.
+#
+# During your first day at WizKidz Inc. you learn the software department is
+# reeling from an automated refactor gone wrong performed by an AI it 
+# has been developing.
+#
+# You are tasked with fixing the faulty programs.
+#
+# Problem:
+# This program normally printed out "Hello World" but now it just does not compile!
+#
+# In Python, functions take any arguments type. 
+#
+# A commonly used type for printing out characters is the `string` type.
+# A string in python is expressed by wrapping text in double quotes `"`. i.e: "hey there!"
+#
+# Try fixing the argument to the fmt.Println function and run `python hello.py`
+# to see if it works.
+
+
+
+def main():
+	# Knowing about the string type, modify
+	# the argument to print to be a single string.
+	print(Hello World)
+
+if __name__ == "__main__":
+    main()
